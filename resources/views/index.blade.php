a6fb686e0c04d1d167e60ba5cc3a54d8bd64ca0c
danielmacapa <71198712+danielmacapa@users.noreply.github.com>
2023-05-16 18:32:05 -0300

    index1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FLCharGen</title>
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <title>GEPE - Greenciador de Personagens</title>
+    <link rel="stylesheet" href="{{ asset('adminlte/css/styleindex.css') }}">
 </head>
 
 <body id="body">
+    <img src="{{ asset('adminlte/img/fundo.jpg') }}">
     <div id="cabecalho">
         <div id="LC">
             <a id="login" href="/forbidden-lands/html/form-login.php">Login</a>
@@ -28,36 +29,3 @@
 </body>
 
 </html>
-
-<!-- Blade
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
-</head>
-
-<body>
-    <h1>GEPE - Gerenciador de Fichas de Personagem</h1>
-    <table>
-        <tr>
-            <td><a href="{{ route('character.list') }}">Lista de Personagens</a></td>
-        </tr>
-        <tr>
-            <td><a href="{{ route('race.list') }}">Lista de Ascendências</a></td>
-        </tr>
-        <tr>
-            <td><a href="{{ route('profession.list') }}">Lista de Profissões</a></td>
-        </tr>
-        <tr>
-            <td><a href="{{ route('player.list') }}">Lista de Jogadores</a></td>
-        </tr>
-    </table>
-</body>
-
-</html>
-FIM Blade -->
